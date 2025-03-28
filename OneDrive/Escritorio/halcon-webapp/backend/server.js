950689ff403d5ea8f063d4af26fa86c8575107ab
OzielMV <Al03048403@tecmilenio.mx>
2025-03-28 14:51:24 -0700

    Configuración inicial del backend con Node.js y Express

---
+++ OneDrive/Escritorio/halcon-webapp/backend/server.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('¡API de Halcon funcionando!');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
